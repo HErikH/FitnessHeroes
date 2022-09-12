ceb2a34ead87885f0a1f4f4811c2d9c0a2ddde4f
HerikH <erik.harutyunyan.03.03@mail.ru>
2022-09-12 11:57:10 +0400

    useRoutes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useRoutes } from 'react-router-dom';
 import './App.scss';
 import Navbar from './components/Navbar'
 import Home from './pages/home/Home'
@@ -11,20 +11,32 @@ import Trainers from './pages/trainers/Trainers'
 import NotFound from './pages/notFound/NotFound'
 import Footer from './components/Footer';
 
-const App = () => (
+const HomePath = () => useRoutes([
+  { path: "/", element: <Home /> },
+  { path: "FitnessHeroes", element: <Home /> },
+  { path: 'about', element: <About /> },
+  { path: 'contact', element: <Contact /> },
+  { path: 'gallery', element: <Gallery /> },
+  { path: 'plans', element: <Plans /> },
+  { path: 'trainers', element: <Trainers /> },
+  { path: '*', element: <NotFound /> },
+])
+function App() {
+  return (
   <div>
     <Navbar />
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path='about' element={<About />} />
+    {/* <Routes> */}
+      <HomePath/>
+      {/* <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='gallery' element={<Gallery />} />
       <Route path='plans' element={<Plans />} />
       <Route path='trainers' element={<Trainers />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+      <Route path='*' element={<NotFound />} /> */}
+    {/* </Routes> */}
     <Footer />
   </div>
-);
+  )
+};
 
 export default App;
