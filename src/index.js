ced4430b415824fa5d685bc48a8973dad051f817
HerikH <erik.harutyunyan.03.03@mail.ru>
2023-11-16 16:51:36 +0400

    index

--- src/index.js
+++ src/index.js
@@ -130,7 +130,7 @@ section {
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <BrowserRouter basename={process.epv.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <ThemeProvider theme={theme}>
     <GlobalStyle />
     <App/>
